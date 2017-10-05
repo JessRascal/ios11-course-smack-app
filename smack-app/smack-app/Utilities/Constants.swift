b1fdcfd0a453bc2ab34d36156cd492bd0cc24e2f
Jess Rascal <jess@justonejess.com>
2017-10-05 22:42:24 +0100

    Lesson 9 complete

--- smack-app/smack-app/Utilities/Constants.swift
+++ smack-app/smack-app/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "http://localhost:3005/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN_VC = "toLoginVC"
 let TO_CREATE_ACC_VC = "toCreateAccVC"
 let UNWIND_TO_CHANNEL_VC = "unwindToChannelVC"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
