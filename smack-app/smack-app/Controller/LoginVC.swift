4f5a34db536fdf1ac36f64411b1e6b2f91baf861
Jess Rascal <jess@justonejess.com>
2017-10-05 20:08:19 +0100

    Lesson 3 complete

---
+++ smack-app/smack-app/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  smack-app
+//
+//  Created by Jess Rascal on 05/10/2017.
+//  Copyright © 2017 jessrascal. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ smack-app/smack-app/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack-app
+//
+//  Created by Jess Rascal on 05/10/2017.
+//  Copyright © 2017 jessrascal. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN_VC = "toLoginVC"
