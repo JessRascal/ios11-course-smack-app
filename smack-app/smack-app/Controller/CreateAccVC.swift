6e43987d281314329cb7f95b34e070d641d6cbe7
Jess Rascal <jess@justonejess.com>
2017-10-06 06:40:30 +0100

    Lesson 10 complete

--- smack-app/smack-app/Controller/CreateAccVC.swift
+++ smack-app/smack-app/Controller/CreateAccVC.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class CreateAccVC: UIViewController {
 
+    
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func createAccPressed(_ sender: Any) {
+        guard let email = emailText.text, emailText.text != "" else { return }
+        guard let pass = passwordText.text, passwordText.text != "" else { return }
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+        
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND_TO_CHANNEL_VC, sender: nil)
     }
