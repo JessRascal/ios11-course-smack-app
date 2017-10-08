c1abf1952f0e2add1d07f50f077ce2792ef625c1
Jess Rascal <jess@justonejess.com>
2017-10-08 19:44:08 +0100

    WIP

---
+++ smack-app/smack-app/Services/SocketService.swift
@@ -0,0 +1,30 @@
+//
+//  SocketService.swift
+//  smack-app
+//
+//  Created by Jess Rascal on 08/10/2017.
+//  Copyright © 2017 jessrascal. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+    var socket: SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+}
